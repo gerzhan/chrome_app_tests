125ef0574598faf5a78d11b21b223894f9b3e689
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2013-12-13 22:12:11 +0700

    update main.js - add "id" property

--- app/main.js
+++ app/main.js
@@ -5,11 +5,12 @@
 chrome.app.runtime.onLaunched.addListener(
   function(){
     chrome.app.window.create('index.html',{
+        id: 'main',
         'bounds': {
             'width':300,
-            'height':500//,
-            //'top':100,
-            //'left':100
+            'height':500,
+            'top':10,
+            'left':10
         },
         minHeight:100,
         minWidth:200
